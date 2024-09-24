60d4efd0053d17793a403747fd7c608498a46532
ldsbdogg <bwilliams@c20g.com>
2024-09-24 11:20:48 -0400

    task: Added the save and reorder button functionalities

--- src/components/EditPipeline/solutionsAdded.tsx
+++ src/components/EditPipeline/solutionsAdded.tsx
@@ -3,17 +3,24 @@ import { Card, CardContent, Button, Box } from '@mui/material'
 
 const SolutionsAdded = (props) => {
   return (
-    <Card sx={{ marginTop: 4, borderRadius: '0px 0px 16px 16px' }} id="zip">
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginRight: 4,
-        }}
-      ></Box>
+    <Card
+      sx={{
+        marginTop: 4,
+        borderRadius: '30px',
+      }}
+      id="zip"
+    >
       <CardContent>
-        <Box display={'flex'} flexDirection={'row'} gap={2} mt={4}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 0,
+            mt: 1,
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
           <Button
             id="reorder"
             color="primary"
@@ -22,6 +29,7 @@ const SolutionsAdded = (props) => {
               borderRadius: '30px',
             }}
             onClick={() => props.onReorderToggler(true)}
+            disabled={props.isReorder}
           >
             Reorder
           </Button>
@@ -32,7 +40,10 @@ const SolutionsAdded = (props) => {
             sx={{
               borderRadius: '30px',
             }}
-            onClick={() => props.onReorderToggler(false)}
+            onClick={() => {
+              props.onSave()
+              props.onReorderToggler(false)
+            }}
           >
             Save
           </Button>

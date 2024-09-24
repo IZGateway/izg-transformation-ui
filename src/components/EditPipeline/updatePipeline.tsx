babde99081f55ef8f1911c1787eaba319a9d0475
ldsbdogg <bwilliams@c20g.com>
2024-09-23 21:05:57 -0400

    feat: created webhook to connect frontend server to backend ui

---
+++ src/components/EditPipeline/updatePipeline.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import https from 'https'
+
+export async function updateData(id, data) {
+  try {
+    const response = await axios.put(`/api/pipelines/${id}`, data, {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error updating data:', error)
+    throw error
+  }
+}

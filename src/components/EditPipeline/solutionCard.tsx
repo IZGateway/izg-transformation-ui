9ba17c655b1358213f299d4fbd6bca3c2ad4fb54
ldsbdogg <bwilliams@c20g.com>
2024-10-07 14:41:31 -0400

    chore removed dynamic numbering and made the sizing dynamic

--- src/components/EditPipeline/solutionCard.tsx
+++ src/components/EditPipeline/solutionCard.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { useCallback, memo, useState } from 'react'
 import {
   Box,
   Card,
@@ -9,62 +9,116 @@ import {
   Typography,
 } from '@mui/material'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
+import SolutionsModal from './Modal/solutionsModal'
+import { useReorderContext } from '../../contexts/EditPipeline/reorderContext'
+import { useUpdatePipeDataContext } from '../../contexts/EditPipeline/updatePipeDataContext'
 
-const SolutionCard = ({ id, solution, index, isReorder, handleDelete }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id })
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
+interface SolutionCardProps {
+  id: string
+  solution: {
+    id: string
+    solutionName: string
+    description: string
+    requestOperations: Array<object>
   }
-
-  const handleDeleteClick = useCallback(
-    (e) => {
-      e.preventDefault()
-      e.stopPropagation()
-      handleDelete(id)
-    },
-    [id, handleDelete]
-  )
-
-  return (
-    <Box
-      ref={setNodeRef}
-      style={style}
-      {...(isReorder ? { ...attributes, ...listeners } : {})}
-    >
-      <Card
-        sx={{
-          height: '100%',
-          borderRadius: '0px 0px 16px 16px',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <CardHeader
-          title={
-            isReorder
-              ? `${index}. ${solution.solutionName}`
-              : solution.solutionName
-          }
-          action={
-            isReorder ? (
-              <IconButton aria-label="delete" onClick={handleDeleteClick}>
-                <DeleteOutlinedIcon />
-              </IconButton>
-            ) : null
-          }
-        />
-        <Divider />
-        <CardContent>
-          <Typography variant="body1">{solution.description}</Typography>
-        </CardContent>
-      </Card>
-    </Box>
-  )
+  index: number
+  preconditions: Array<{
+    method: string
+    id: string
+    dataPath: string
+    regex: string
+    comparisonValue: string
+  }>
 }
+
+const SolutionCard = memo(
+  ({ id, solution, index, preconditions }: SolutionCardProps) => {
+    const {
+      attributes,
+      listeners,
+      setNodeRef,
+      transform,
+      transition,
+      isDragging,
+    } = useSortable({ id })
+    const { isReorder } = useReorderContext()
+    const { pipeData, setPipeData } = useUpdatePipeDataContext()
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const handleDelete = useCallback(() => {
+      const newOrder = pipeData.filter((pipe) => pipe.id !== id)
+      setPipeData(newOrder)
+    }, [id, pipeData, setPipeData])
+
+    return (
+      <Box
+        ref={setNodeRef}
+        style={{
+          transform: CSS.Transform.toString(transform),
+          transition,
+          zIndex: isDragging ? 1000 : 'auto',
+        }}
+        {...(isReorder && { ...attributes, ...listeners })}
+        sx={{ height: '100%' }}
+      >
+        <Card
+          sx={{
+            height: '100%',
+            borderRadius: '0px 0px 16px 16px',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <CardHeader
+            title={
+              <Typography
+                variant="h5"
+                noWrap
+                sx={{ fontSize: '1.4em', whiteSpace: 'nowrap' }}
+              >
+                {`${index}. ${solution.solutionName}`}
+              </Typography>
+            }
+            action={
+              <>
+                {isReorder && (
+                  <IconButton aria-label="delete" onClick={handleDelete}>
+                    <DeleteOutlinedIcon />
+                  </IconButton>
+                )}
+                <IconButton
+                  aria-label="edit"
+                  onClick={() => {
+                    setIsModalOpen(true)
+                  }}
+                >
+                  <EditOutlinedIcon />
+                </IconButton>
+              </>
+            }
+          />
+          <Divider />
+          <CardContent>
+            <Typography variant="body1">{solution.description}</Typography>
+          </CardContent>
+        </Card>
+        {isModalOpen && (
+          <SolutionsModal
+            selectedSolution={solution}
+            isNewSolution={preconditions.length === 0}
+            existingPreconditions={preconditions}
+            setOpen={setIsModalOpen}
+            open={isModalOpen}
+          />
+        )}
+      </Box>
+    )
+  }
+)
+
+SolutionCard.displayName = 'SolutionCard'
 
 export default SolutionCard

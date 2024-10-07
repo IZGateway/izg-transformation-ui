327062a62d88c5b0e0e63ee77bc0a1600162929c
ldsbdogg <bwilliams@c20g.com>
2024-10-07 14:38:48 -0400

    chore: cleaned up code and updated text blocks

--- src/components/EditPipeline/index.tsx
+++ src/components/EditPipeline/index.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react'
 import Container from '../../components/Container'
 import ErrorBoundary from '../../components/ErrorBoundary'
 import { useRouter } from 'next/router'
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback, ChangeEvent } from 'react'
 import Close from '../Close'
 import EditIcon from '@mui/icons-material/Edit'
 import {
@@ -18,185 +17,147 @@ import {
 } from '@mui/material'
 import Settings from './settings'
 import SolutionsList from './solutionsList'
-import SolutionsAdded from './solutionsAdded'
+import SolutionsModified from './solutionsModifed'
 import CloseIcon from '@mui/icons-material/Close'
 import SolutionsGrid from './solutionsGrid'
+import ReorderProvider from '../../contexts/EditPipeline/reorderContext'
+import { updateData } from '../../components/EditPipeline/updatePipeline'
+import { usePipelineDataContext } from '../../contexts/EditPipeline/pipelineDataContext'
+import { useUpdatePipeDataContext } from '../../contexts/EditPipeline/updatePipeDataContext'
 
-const EditPipeline = ({
-  pipeData,
-  orgData,
-  solutionsData,
-  setPipeData,
-  preconditionsData,
-  preconditionMethodsData,
-}) => {
+const EditPipeline = ({ orgData }) => {
   const router = useRouter()
   const { isReady, query } = router
-  const [description, setDescription] = useState(pipeData.description)
+  const { pipelineData } = usePipelineDataContext()
+  const { pipeData } = useUpdatePipeDataContext()
+  const [description, setDescription] = useState(pipelineData.description)
   const [open, setOpen] = useState(false)
-  const [isReorder, setIsReorder] = useState(false)
-  const [currentOrder, setCurrentOrder] = useState(pipeData.pipes)
 
   useEffect(() => {
     if (!isReady) return
   }, [isReady, query])
 
-  const updatePipelineData = useCallback(
-    (newOrder) => {
-      setCurrentOrder(newOrder)
-      setPipeData((prevPipeData) => ({
-        ...prevPipeData,
-        pipes: newOrder,
-      }))
-    },
-    [setPipeData]
-  )
-
-  const handleDelete = useCallback(
-    (id) => {
-      const newOrder = currentOrder.filter((pipe) => pipe.id !== id)
-      updatePipelineData(newOrder)
-    },
-    [currentOrder, updatePipelineData]
-  )
-
   const handleSave = useCallback(async () => {
-    try {
-      updatePipelineData(currentOrder)
-      setIsReorder(false)
-    } catch (error) {
-      console.error('Error updating pipeline order:', error)
-    }
-  }, [currentOrder, updatePipelineData])
+    await updateData(query.id as string, {
+      ...pipelineData,
+      pipes: pipeData,
+    })
+  }, [pipeData, query, pipelineData])
 
   return !isReady ? (
     <>Loading....</>
   ) : (
-    <Container title="Pipeline">
-      <ErrorBoundary>
-        <Box sx={{ position: 'relative' }}>
-          <div>
-            <Close />
-            <>
-              <Box sx={{ marginTop: 4 }}>
-                <Typography
-                  variant="h1"
-                  fontWeight={700}
-                  fontSize="32px"
-                  id="title-pipeline"
-                >
-                  {pipeData.pipelineName} Pipeline
-                </Typography>
-                {open ? (
-                  <Box display={'flex'} flexDirection={'row'}>
-                    <TextField
-                      id="pipeline-description"
-                      label="Pipeline Description"
-                      variant="standard"
-                      size="small"
-                      value={description}
-                      onChange={(
-                        event: React.ChangeEvent<HTMLInputElement>
-                      ) => {
-                        setDescription(event.target.value)
-                      }}
-                    />
-                    <IconButton
-                      aria-label="close"
-                      color="primary"
-                      onClick={() => {
-                        setOpen(false)
-                      }}
-                    >
-                      <CloseIcon fontSize="small" />
-                    </IconButton>
-                  </Box>
-                ) : (
-                  <Box display={'flex'} flexDirection={'row'}>
-                    <Typography variant="body1">
-                      {pipeData.description} 25/75 Characters
-                    </Typography>
-
-                    <Tooltip
-                      arrow
-                      placement="bottom"
-                      title="Edit description of the pipeline"
-                    >
+    <ReorderProvider>
+      <Container title="Pipeline">
+        <ErrorBoundary>
+          <Box sx={{ position: 'relative' }}>
+            <div>
+              <Close />
+              <>
+                <Box sx={{ marginTop: 4 }}>
+                  <Typography
+                    variant="h1"
+                    fontWeight={700}
+                    fontSize="32px"
+                    id="title-pipeline"
+                  >
+                    {pipelineData.pipelineName} Pipeline
+                  </Typography>
+                  {open ? (
+                    <Box display={'flex'} flexDirection={'row'}>
+                      <TextField
+                        id="pipeline-description"
+                        label="Pipeline Description"
+                        variant="standard"
+                        size="small"
+                        value={description}
+                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                          setDescription(event.target.value)
+                        }}
+                      />
                       <IconButton
-                        aria-label="edit"
+                        aria-label="close"
                         color="primary"
                         onClick={() => {
-                          setOpen(true)
+                          setOpen(false)
                         }}
                       >
-                        <EditIcon fontSize="small" />
+                        <CloseIcon fontSize="small" />
                       </IconButton>
-                    </Tooltip>
-                  </Box>
-                )}
-              </Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  gap: 4,
-                  flexDirection: 'row',
-                  alignItems: 'flex-start',
-                  marginTop: 4,
-                }}
-              >
-                <Settings pipeData={pipeData} orgData={orgData} />
+                    </Box>
+                  ) : (
+                    <Box display={'flex'} flexDirection={'row'}>
+                      <Typography variant="body1">
+                        {pipelineData.description} 25/75 Characters
+                      </Typography>
 
-                <Box sx={{ width: '70%' }}>
-                  <SolutionsList
-                    solutionsData={solutionsData}
-                    pipeData={currentOrder}
-                    onAddSolution={updatePipelineData}
-                    setIsReorder={setIsReorder}
-                    preconditionsData={preconditionsData}
-                    preconditionMethodsData={preconditionMethodsData}
-                  />
-                  {currentOrder.length > 0 && (
-                    <>
-                      <Card
-                        sx={{
-                          minWidth: 275,
-
-                          borderRadius: '0px 0px 30px 30px',
-                          marginTop: 4,
-                        }}
+                      <Tooltip
+                        arrow
+                        placement="bottom"
+                        title="Edit description of the pipeline"
                       >
-                        <CardHeader title="Configured Solutions" />
-                        <Divider />
-                        <CardContent>
-                          <Typography variant="body1">
-                            Once you have adjusted your settings add a solution.
-                            You can add as many you like, please note the order
-                            is sequential.
-                          </Typography>
-                        </CardContent>
-                      </Card>
-
-                      <SolutionsGrid
-                        pipeData={currentOrder}
-                        solutionsData={solutionsData}
-                        isReorder={isReorder}
-                        onOrderChange={updatePipelineData}
-                        handleDelete={handleDelete}
-                      />
-                    </>
+                        <IconButton
+                          aria-label="edit"
+                          color="primary"
+                          onClick={() => {
+                            setOpen(true)
+                          }}
+                        >
+                          <EditIcon fontSize="small" />
+                        </IconButton>
+                      </Tooltip>
+                    </Box>
                   )}
-                  <SolutionsAdded
-                    isReorder={isReorder}
-                    onReorderToggler={setIsReorder}
-                    onSave={handleSave}
-                  />
                 </Box>
-              </Box>
-            </>
-          </div>
-        </Box>
-      </ErrorBoundary>
-    </Container>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    gap: 4,
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    marginTop: 4,
+                  }}
+                >
+                  <Settings pipeData={pipelineData} orgData={orgData} />
+
+                  <Box sx={{ width: '70%' }}>
+                    <SolutionsList />
+                    {pipeData && pipeData.length > 0 && (
+                      <>
+                        <Card
+                          sx={{
+                            minWidth: 275,
+
+                            borderRadius: '0px 0px 30px 30px',
+                            marginTop: 4,
+                          }}
+                        >
+                          <CardHeader title="Configured Solutions" />
+                          <Divider />
+                          <CardContent>
+                            <Typography variant="body1">
+                              Your added solutions are listed below. You can add
+                              or remove as many you like.{' '}
+                              <b style={{ textDecoration: 'underline' }}>
+                                Please note, the order of operations is
+                                sequential.
+                              </b>
+                            </Typography>
+                          </CardContent>
+                        </Card>
+
+                        <SolutionsGrid />
+                      </>
+                    )}
+                    <SolutionsModified handleSave={handleSave} />
+                  </Box>
+                </Box>
+              </>
+            </div>
+          </Box>
+        </ErrorBoundary>
+      </Container>
+    </ReorderProvider>
   )
 }
 

48a45ce44ea8e4f338b9b8a8d07e30c813d26999
ldsbdogg <bwilliams@c20g.com>
2024-10-07 14:50:16 -0400

    added useFormattedPreconditions and cleaned up code

--- src/components/EditPipeline/Modal/utils.tsx
+++ src/components/EditPipeline/Modal/utils.tsx
@@ -1,13 +1,17 @@
 export const updatePrecondition = (
-  index,
-  field,
-  value,
-  preconditions,
-  setPreconditions
+  index: number,
+  field: string,
+  value: string,
+  preconditions: Array<{ [key: string]: string }>,
+  setPreconditions: React.Dispatch<
+    React.SetStateAction<Array<{ [key: string]: string }>>
+  >
 ) => {
-  const newPreconditions = [...preconditions]
-  newPreconditions[index][field] = value
-  setPreconditions(newPreconditions)
+  setPreconditions((prevPreconditions) => {
+    const newPreconditions = [...prevPreconditions]
+    newPreconditions[index] = { ...newPreconditions[index], [field]: value }
+    return newPreconditions
+  })
 }
 
 export const removePrecondition = (index, preconditions, setPreconditions) => {
@@ -20,32 +24,53 @@ export const transformPreconditions = (
   preconditionMethodsData,
   preconditions
 ) => {
-  return preconditions.map((precondition) => {
-    const transformedPrecondition = {
-      id: precondition.id,
-      method: precondition.method,
-      dataPath: '',
-    }
-
-    const matchingPrecondition = preconditionsData.data.find(
-      (p) => p.id === precondition.id
-    )
-    console.log(matchingPrecondition)
-    transformedPrecondition.dataPath = matchingPrecondition.dataPath
-
-    const methodData = preconditionMethodsData.find(
-      (m) => m.method === precondition.method
-    )
-
-    if (methodData) {
-      const additionalProp = Object.keys(methodData.properties).find(
-        (key) => key !== 'id' && key !== 'datapath'
-      )
-      if (additionalProp) {
-        transformedPrecondition[additionalProp] = precondition.value
+  return preconditions.map(
+    (precondition: { id: string; method: string; value: string }) => {
+      const transformedPrecondition = {
+        id: precondition.id,
+        method: precondition.method,
+        dataPath: '',
       }
-      console.log(precondition)
+
+      const matchingPrecondition = preconditionsData.data.find(
+        (p) => p.id === precondition.id
+      )
+      transformedPrecondition.dataPath = matchingPrecondition.dataPath
+
+      const methodData = preconditionMethodsData.find(
+        (m) => m.method === precondition.method
+      )
+
+      if (methodData) {
+        const additionalProp = Object.keys(methodData.properties).find(
+          (key) => key !== 'id' && key !== 'dataPath'
+        )
+        console.log('additionalProp', additionalProp)
+        if (additionalProp) {
+          transformedPrecondition[additionalProp] = precondition.value
+        }
+      }
+      return transformedPrecondition
     }
-    return transformedPrecondition
+  )
+}
+
+export const useFormattedPreconditions = (
+  hasPreconditions: boolean,
+  existingPreconditions: Array<{ [key: string]: string }>
+): Array<{ id: string; method: string; value: string }> => {
+  if (!hasPreconditions) return []
+  if (existingPreconditions.length === 0 || !existingPreconditions) {
+    return [{ id: '', method: '', value: '' }]
+  }
+  return existingPreconditions.map((precondition) => {
+    const valueKey = Object.keys(precondition).find(
+      (key) => !['id', 'method', 'dataPath'].includes(key)
+    )
+    return {
+      id: precondition.id || '',
+      method: precondition.method || '',
+      value: precondition[valueKey] || '',
+    }
   })
 }

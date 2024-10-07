ea45a603a3470b64b0f33f6e294b2af9dfedba01
ldsbdogg <bwilliams@c20g.com>
2024-10-07 14:47:02 -0400

    added a disabled requirement to the textbox and cleaned up code for greater clarity

--- src/components/EditPipeline/Modal/preconditionsSection.tsx
+++ src/components/EditPipeline/Modal/preconditionsSection.tsx
@@ -20,99 +20,85 @@ const PreconditionsSection = ({
   preconditionMethodsData,
   preconditionsData,
 }) => {
+  const renderFormControl = (index, field, value, label, data) => (
+    <FormControl sx={{ width: '25%' }}>
+      <InputLabel shrink={false}>{value ? '' : label}</InputLabel>
+      <Select
+        value={value}
+        onChange={(e) =>
+          updatePrecondition(
+            index,
+            field,
+            e.target.value,
+            preconditions,
+            setPreconditions
+          )
+        }
+        displayEmpty
+        required
+      >
+        {data.map((item) => (
+          <MenuItem key={item.id || item.method} value={item.id || item.method}>
+            {item.fieldName || item.method}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  )
+
   return (
     <Box sx={styles.preconditionsContainer}>
-      {preconditions.map((precondition, index) => (
-        <React.Fragment key={index}>
-          {index > 0 && <Divider sx={styles.divider} />}
-          <FormGroup row sx={styles.preconditionRow}>
-            <FormControl sx={{ width: '25%' }}>
-              <InputLabel id="precondition-id-label" shrink={false}>
-                {precondition.id ? '' : 'Field'}
-              </InputLabel>
-              <Select
-                value={precondition.id}
-                id={`precondition-id-${index}`}
-                onChange={(e) =>
-                  updatePrecondition(
-                    index,
-                    'id',
-                    e.target.value,
-                    preconditions,
-                    setPreconditions
-                  )
+      {preconditions.length > 0 &&
+        preconditions.map((precondition, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && <Divider sx={styles.divider} />}
+            <FormGroup row sx={styles.preconditionRow}>
+              {renderFormControl(
+                index,
+                'id',
+                precondition.id,
+                'Field',
+                preconditionsData.data
+              )}
+              {renderFormControl(
+                index,
+                'method',
+                precondition.method,
+                'Conditional',
+                preconditionMethodsData
+              )}
+              <FormControl sx={{ width: '25%' }}>
+                <TextField
+                  value={precondition.value || ''}
+                  label={precondition.value ? '' : 'Value'}
+                  InputLabelProps={{ shrink: false }}
+                  onChange={(e) =>
+                    updatePrecondition(
+                      index,
+                      'value',
+                      e.target.value,
+                      preconditions,
+                      setPreconditions
+                    )
+                  }
+                  placeholder="Value"
+                  required
+                  disabled={['exists', 'not_exists'].includes(
+                    precondition.method
+                  )}
+                />
+              </FormControl>
+              <IconButton
+                onClick={() =>
+                  removePrecondition(index, preconditions, setPreconditions)
                 }
-                displayEmpty
-                required
+                aria-label="delete"
               >
-                {preconditionsData.data.map((preconditionItem) => (
-                  <MenuItem
-                    key={preconditionItem.id}
-                    value={preconditionItem.id}
-                  >
-                    {preconditionItem.fieldName}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <FormControl sx={{ width: '25%' }}>
-              <InputLabel id="precondition-method-label" shrink={false}>
-                {precondition.method ? '' : 'Conditional'}
-              </InputLabel>
-              <Select
-                value={precondition.method}
-                id={`precondition-method-${index}`}
-                onChange={(e) =>
-                  updatePrecondition(
-                    index,
-                    'method',
-                    e.target.value,
-                    preconditions,
-                    setPreconditions
-                  )
-                }
-                displayEmpty
-                required
-              >
-                {preconditionMethodsData.map((method) => (
-                  <MenuItem key={method.method} value={method.method}>
-                    {method.method}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <FormControl sx={{ width: '25%' }}>
-              <TextField
-                value={precondition.value}
-                id={`precondition-value-${index}`}
-                label={precondition.value ? '' : 'Value'}
-                InputLabelProps={{ shrink: false }}
-                onChange={(e) =>
-                  updatePrecondition(
-                    index,
-                    'value',
-                    e.target.value,
-                    preconditions,
-                    setPreconditions
-                  )
-                }
-                placeholder="Value"
-                required
-              />
-            </FormControl>
-            <IconButton
-              onClick={() =>
-                removePrecondition(index, preconditions, setPreconditions)
-              }
-              aria-label="delete"
-            >
-              <DeleteOutlinedIcon sx={{ color: 'red' }} />
-            </IconButton>
-          </FormGroup>
-        </React.Fragment>
-      ))}
+                <DeleteOutlinedIcon sx={{ color: 'red' }} />
+              </IconButton>
+            </FormGroup>
+          </React.Fragment>
+        ))}
     </Box>
   )
 }

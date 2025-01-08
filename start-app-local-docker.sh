66e76e328dd72e74805f8edb41d36527f8e997d8
Austin Moody <amoody@ainq.com>
2025-01-08 14:14:16 -0500

    Update package.json so that start's for local-dev and local-docker call the new start-app-local-docker.sh script instead of running docker-compose directly.  This is because docker compose file now specifies a network and the shell script will check for and create that network if it does not exist.
    Rename docker-compose.local.yml to docker-compose.yml.  docker-compose.yml contains no sensitive information and naming as base docker-compose.yml means you don't have to specify flags when running docker-compose, it will just automatically look for that file.
    
    Update start-app-local-docker.sh so that a dev flag can be sent in to execute npm run dev in order to mimic what was in package.json for start:local-dev.

--- start-app-local-docker.sh
+++ start-app-local-docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Create a shared network if it does not exist
 NETWORK_NAME="izg-shared-network"
+RUN_DEV="NO"
 
+# Check to see if user wants to run npm dev mode
+if [ $# -eq 1 ] && [ "$1" == "DEV" ]; then RUN_DEV="YES"; fi
+
+# Create a shared network if it does not exist
 if ! docker network ls | grep -q "$NETWORK_NAME"; then
   echo "Network $NETWORK_NAME does not exist. Creating it..."
   docker network create "$NETWORK_NAME"
@@ -11,4 +15,7 @@ else
 fi
 
 # Run docker-compose
-docker-compose -f docker-compose.local.yml up -d
+docker-compose up -d
+if [ "$RUN_DEV" == "YES" ]; then
+  npm run dev
+fi

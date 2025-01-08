8568bcc2010945b587f3770c481a9014c5a2acd7
Austin Moody <amoody@ainq.com>
2025-01-07 21:03:31 -0500

    Update FetchDataFromEndpoint.tsx to take into account new env setting on if we should use certificates or not.

--- src/pages/api/serverside/FetchDataFromEndpoint.tsx
+++ src/pages/api/serverside/FetchDataFromEndpoint.tsx
@@ -3,33 +3,39 @@ import path from 'path'
 import axios from 'axios'
 import https from 'https'
 
-const fetchDataFromEndpoint = async (endpoint, access_token : string) => {
+const fetchDataFromEndpoint = async (endpoint : string, access_token : string) => {
   const XFORM_SERVICE_ENDPOINT_CRT_PATH =
     process.env.XFORM_SERVICE_ENDPOINT_CRT_PATH || ''
   const XFORM_SERVICE_ENDPOINT_KEY_PATH =
     process.env.XFORM_SERVICE_ENDPOINT_KEY_PATH || ''
   const XFORM_SERVICE_ENDPOINT_PASSCODE =
     process.env.XFORM_SERVICE_ENDPOINT_PASSCODE || ''
-  const httpsAgentOptions = {
-    cert: fs.readFileSync(
-      path.resolve(XFORM_SERVICE_ENDPOINT_CRT_PATH),
-      'utf-8'
-    ),
-    key: fs.readFileSync(
-      path.resolve(XFORM_SERVICE_ENDPOINT_KEY_PATH),
-      'utf-8'
-    ),
-    passphrase: XFORM_SERVICE_ENDPOINT_PASSCODE,
+  const XFORM_SERVICE_ENDPOINT_USE_CERT : boolean = process.env.XFORM_SERVICE_ENDPOINT_USE_CERT === 'true'
+
+  const httpsAgentOptions: https.AgentOptions = {
     rejectUnauthorized: false,
     keepAlive: true,
   }
+
+  if (XFORM_SERVICE_ENDPOINT_USE_CERT) {
+    httpsAgentOptions.cert = fs.readFileSync(
+      path.resolve(XFORM_SERVICE_ENDPOINT_CRT_PATH),
+      'utf-8'
+    )
+
+    httpsAgentOptions.key = fs.readFileSync(
+      path.resolve(XFORM_SERVICE_ENDPOINT_KEY_PATH),
+      'utf-8'
+    )
+
+    httpsAgentOptions.passphrase = XFORM_SERVICE_ENDPOINT_PASSCODE
+  }
+
   try {
+    const headers = XFORM_SERVICE_ENDPOINT_USE_CERT ? {} : { Authorization: `Bearer ${access_token}` }
     const response = await axios.get(endpoint, {
       httpsAgent: new https.Agent(httpsAgentOptions),
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        'X-Not-Admin': 'true'
-      },
+      headers,
       timeout: 30000,
     })
     return response.data

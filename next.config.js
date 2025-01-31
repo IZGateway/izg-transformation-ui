58b6482ad203be974c1a4bd2d1395d00dd571381
Austin Moody <amoody@ainq.com>
2025-01-31 09:27:20 -0500

    Add images.unoptimized = true to next.config.js in order to remediate CVE-2024-47831 being seen in AWS ECR.

--- next.config.js
+++ next.config.js
@@ -10,6 +10,34 @@ module.exports = async (phase, { defaultConfig }) => {
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
+    images: {
+      unoptimized: true
+    },
+    headers: async () => {
+      return [
+        {
+          source: '/:path*{/}?',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'cache-control',
+              value: 'no-cache, no-store, must-revalidate',
+            },
+          ],
+        },
+      ]
+    },
     // Disable webpack cache in production
     generateBuildId: async () => {
       return process.env.BUILD_ID || 'development'

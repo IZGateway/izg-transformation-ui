5ff48739d59d14dd1e80a3ef1ed9f97c4d995370
mattystank <32516464+mattystank@users.noreply.github.com>
2025-02-10 06:45:47 -0500

    feat: add version footer to application
    IGDD-1890
    
    * ui: create base footer off of CC model
    
    * ui: add footer to manage table/page
    
    * fix: comment out CDC Authorized text
    
    * ui: ajust footer zindex and margins for connectionTable

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+import pack from '../../../package.json'
+
+const Footer: React.FC = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 2,
+        px: 4,
+        position: 'fixed',
+        bottom: 0,
+        ml: -4,
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        justifyContent: 'space-between',
+        top: 'auto',
+        zIndex: 1000,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}
+    >
+      <Typography variant="caption" align="left">
+        Version {pack.version}-{process.env.NEXT_PUBLIC_BUILD_ID || 'Dev'} |
+        Immunization (IZ) Gateway Transformation Service 2024
+      </Typography>
+      {/* <Typography variant="caption" mr={10}>
+        This application has been authorized by the Centers for Disease Control
+        and Prevention
+      </Typography> */}
+    </Box>
+  )
+}
+
+export default Footer
